92fa805d33ffd3c780a5122bc929ccaffb02d932
madrinejean123 <jeanmadrinenamulinde@gmail.com>
2025-01-17 12:19:48 +0300

    changes

--- new_app/lib/screens/auth/views/signup_screen.dart
+++ new_app/lib/screens/auth/views/signup_screen.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter_icons/flutter_icons.dart'; // Import the flutter_icons package
+import 'package:shop/constants.dart';
+import 'package:shop/screens/auth/views/components/sign_up_form.dart';
 import 'package:shop/route/route_constants.dart';
-
-import '../../../constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -13,16 +14,29 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  // Google Sign-In method
   Future<void> _signInWithGoogle() async {
     try {
-      await _googleSignIn.signIn();
-      if (!mounted) return;
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser!.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+      await _auth.signInWithCredential(credential);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text('Signed in as: ${_auth.currentUser?.displayName}')),
+      );
       Navigator.pushNamed(context, entryPointScreenRoute);
     } catch (e) {
-      print("Google sign-in error: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error signing in: $e')),
+      );
     }
   }
 
@@ -33,7 +47,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: Column(
           children: [
             Image.asset(
-              "assets/images/image copy 2.png",
+              "assets/images/signUp_dark.png",
               height: MediaQuery.of(context).size.height * 0.35,
               width: double.infinity,
               fit: BoxFit.cover,
@@ -49,24 +63,62 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   ),
                   const SizedBox(height: defaultPadding / 2),
                   const Text(
-                    "Please sign up using your Google account.",
-                  ),
-                  const SizedBox(height: defaultPadding * 2),
-                  // Google Sign Up Button with Google icon from flutter_icons
-                  ElevatedButton.icon(
-                    onPressed: _signInWithGoogle,
-                    icon: const Icon(
-                        FlutterIcons.google_ant), // Using Google icon
-                    label: const Text("Sign Up with Google"),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(double.infinity, 50),
-                    ),
+                    "Please enter your valid data in order to create an account.",
                   ),
                   const SizedBox(height: defaultPadding),
+                  SignUpForm(formKey: _formKey),
+                  const SizedBox(height: defaultPadding),
+                  Row(
+                    children: [
+                      Checkbox(
+                        onChanged: (value) {},
+                        value: false,
+                      ),
+                      Expanded(
+                        child: Text.rich(
+                          TextSpan(
+                            text: "I agree with the",
+                            children: [
+                              TextSpan(
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    Navigator.pushNamed(
+                                        context, termsOfServicesScreenRoute);
+                                  },
+                                text: " Terms of service ",
+                                style: const TextStyle(
+                                  color: primaryColor,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              const TextSpan(
+                                text: "& privacy policy.",
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: defaultPadding * 2),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, entryPointScreenRoute);
+                    },
+                    child: const Text("Continue"),
+                  ),
+                  const SizedBox(height: defaultPadding),
+                  ElevatedButton.icon(
+                    onPressed: _signInWithGoogle,
+                    icon: Icon(Icons.login),
+                    label: const Text("Sign in with Google"),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.redAccent),
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Text("Already have an account?"),
+                      const Text("Do you have an account?"),
                       TextButton(
                         onPressed: () {
                           Navigator.pushNamed(context, logInScreenRoute);
@@ -77,7 +129,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   ),
                 ],
               ),
-            ),
+            )
           ],
         ),
       ),
